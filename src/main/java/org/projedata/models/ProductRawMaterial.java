be5863fc58950df7234bb1c1fcdd44e0764b8cd0
Gabriel <gabriel_.brito@hotmail.com>
2026-02-23 22:44:19 -0300

    Feat: Create Entity

---
+++ src/main/java/org/projedata/models/ProductRawMaterial.java
@@ -0,0 +1,64 @@
+package org.projedata.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_product_raw_materials")
+public class ProductRawMaterial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "raw_material_id", nullable = false)
+    private RawMaterial rawMaterial;
+
+    @Column(name = "quantity_needed", nullable = false)
+    private Integer quantityNeeded;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public RawMaterial getRawMaterial() {
+        return rawMaterial;
+    }
+
+    public void setRawMaterial(RawMaterial rawMaterial) {
+        this.rawMaterial = rawMaterial;
+    }
+
+    public Integer getQuantityNeeded() {
+        return quantityNeeded;
+    }
+
+    public void setQuantityNeeded(Integer quantityNeeded) {
+        this.quantityNeeded = quantityNeeded;
+    }
+
+}
